eadf19f09f6f8e48c6f6e71fe564293537fc518d
NenyUX <nenyux.dev@gmail.com>
2026-07-18 00:30:19 -0400

    Update setup_edl.sh
    fix

--- setup_edl.sh
+++ setup_edl.sh
@@ -1,39 +1,32 @@
 #!/bin/bash
+[ "$EUID" -ne 0 ] && exit 1
+DIR="/opt/edl"
 
-if [ "$EUID" -ne 0 ]; then
-    echo "Ejecuta con sudo."
-    exit 1
-fi
-
-echo "[*] Preparando sistema..."
-
-if [ -f /etc/debian_version ]; then
-    apt update
-    apt install -y adb fastboot python3-dev python3-pip liblzma-dev git
+if [ -f /etc/arch-release ]; then
+    pacman -Syu --noconfirm android-tools python python-pip git xz
+    pacman -Rns --noconfirm modemmanager || true
+elif [ -f /etc/debian_version ]; then
+    apt update && apt install -y adb fastboot python3-dev python3-pip liblzma-dev git
     apt purge -y modemmanager
 elif [ -f /etc/fedora-release ]; then
     dnf install -y adb fastboot python3-devel python3-pip xz-devel git
-elif [ -f /etc/arch-release ]; then
-    pacman -Syu --noconfirm android-tools python python-pip git xz
-    pacman -Rns --noconfirm modemmanager
 fi
 
 systemctl stop ModemManager
 systemctl disable ModemManager
 
-INSTALL_DIR="/opt/edl"
-mkdir -p $INSTALL_DIR
-
-echo "[*] Descargando edl..."
-cd $INSTALL_DIR
-git clone https://github.com/bkerler/edl.git .
+mkdir -p "$DIR"
+git clone https://github.com/bkerler/edl.git "$DIR"
+cd "$DIR"
 git submodule update --init --recursive
 
-echo "[*] Instalando..."
-pip3 install -r requirements.txt
-pip3 install .
+python3 -m venv venv
+./venv/bin/pip install --upgrade pip
+./venv/bin/pip install -r requirements.txt
+./venv/bin/pip install .
 
 chmod +x autoinstall.sh
 ./autoinstall.sh
 
-echo "[*] Instalación completada."
+ln -sf "$DIR/venv/bin/edl" /usr/local/bin/edl
+chmod +x /usr/local/bin/edl
